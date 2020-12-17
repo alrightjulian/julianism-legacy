57ba7fc10f07ea8fea67059ded24847705c82ece
Julian <julianvargkim@gmail.com>
2020-12-16 21:18:46 -0800

    Updated AWS transcript link.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const LINKED_IN_LINK = "https://www.linkedin.com/in/juliankimca/";
 export const GITHUB_LINK = "https://github.com/julianvargkim";
 export const AWS_TRANSCRIPT_LINK =
-  "https://www.certmetrics.com/amazon/public/transcript.aspx?transcript=BQ7TLRDK1MEQ1B9E";
+  "https://www.youracclaim.com/badges/8686eb0f-e4a1-4201-a046-adb3cc0e6f5b/public_url";
 export const SPOTIFY_PLAYLIST_LINK =
   "https://open.spotify.com/playlist/0e9tgzSF2LRIOkyJogVrpQ?si=tvn8vY1JRAWdOaj0hHwjLw";
