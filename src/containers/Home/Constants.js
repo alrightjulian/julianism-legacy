348b2dda0121cbc6dd598016d84146eeaf41b662
Julian <julianvargkim@gmail.com>
2020-12-21 21:10:18 -0800

    Constants organized.

--- src/containers/Home/Constants.js
+++ src/containers/Home/Constants.js
@@ -1,35 +1,47 @@
-export const PROGRAMMING_TITLE = "Software engineer by profession";
-export const PROGRAMMING_SECTIONS = [
-  <>
-    I have about a year of professional experience as a software engineer. I've
-    worked with <b>AngularJS</b>, <b>Grails</b> using <b>Java</b> and{" "}
-    <b>Groovy</b>. I'm also an <b>AWS Certified Cloud Practitioner</b>.
-  </>,
-  <>
-    In my spare time, I enjoy learning other exciting technologies on the side.
-    I've been enjoying <b>React</b> and <b>Serverless Architecture</b>.
-  </>,
-  <>I'm currently looking for exciting opportunities and offers.</>,
-];
+import programmingImage from "../../images/programming.svg";
+import musicImage from "../../images/music.svg";
+import financeImage from "../../images/finance.svg";
 
-export const MUSIC_TITLE = "Guitarist at heart";
-export const MUSIC_SECTIONS = [
-  <>
-    I have been playing <b>guitar</b> and <b>ukulele</b> for about 5 years.
-    Making my own album is one of my life-long goals.
-  </>,
-  <>
-    I love just about any genre but my favorites are desert rock, jazz and doom
-    metal.
-  </>,
-];
-
-export const FINANCE_TITLE = "Personal finance enthusiast";
-export const FINANCE_SECTIONS = [
-  <>
-    I enjoy learning about <b>personal finance</b>. I value well-planned
-    budgeting, spending and investing in my personal life.
-  </>,
-];
-
-export const CONTACT_LINK = "https://forms.gle/XXnMuwVbGtfKj2vz8";
+export const INFO_CARD_DATA = {
+  programming: {
+    title: "Software engineer by profession",
+    sections: [
+      <>
+        I have about a year of professional experience as a software engineer.
+        I've worked with <b>AngularJS</b>, <b>Grails</b> using <b>Java</b> and{" "}
+        <b>Groovy</b>. I'm also an <b>AWS Certified Cloud Practitioner</b>.
+      </>,
+      <>
+        In my spare time, I enjoy learning other exciting technologies on the
+        side. I've been enjoying <b>React</b> and <b>Serverless Architecture</b>
+        .
+      </>,
+      <>I'm currently looking for exciting opportunities and offers.</>,
+    ],
+    image: programmingImage,
+  },
+  music: {
+    title: "Guitarist at heart",
+    sections: [
+      <>
+        I have been playing <b>guitar</b> and <b>ukulele</b> for about 5 years.
+        Making my own album is one of my life-long goals.
+      </>,
+      <>
+        I love just about any genre but my favorites are desert rock, jazz and
+        doom metal.
+      </>,
+    ],
+    image: musicImage,
+  },
+  finance: {
+    title: "Personal finance enthusiast",
+    sections: [
+      <>
+        I enjoy learning about <b>personal finance</b>. I value well-planned
+        budgeting, spending and investing in my personal life.
+      </>,
+    ],
+    image: financeImage,
+  },
+};
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const AWS_TRANSCRIPT_LINK =
   "https://www.youracclaim.com/badges/8686eb0f-e4a1-4201-a046-adb3cc0e6f5b/public_url";
 export const SPOTIFY_PLAYLIST_LINK =
   "https://open.spotify.com/playlist/0e9tgzSF2LRIOkyJogVrpQ?si=tvn8vY1JRAWdOaj0hHwjLw";
+export const CONTACT_LINK = "https://forms.gle/XXnMuwVbGtfKj2vz8";
